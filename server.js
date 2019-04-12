8d2e7d0c8fd9d25f25d8a7f66aa7864288a8e499
ViniciusGPereira <vinicius.pereira.job@gmail.com>
2019-04-11 22:29:04 -0300

    Respondendo ao navegador
    Função GET, a qual retorna uma mensagem ao navegador quando tenta uma conexão com o servidor

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+// Iniciando servidor na porta 3000
 app.listen(3000, function(){
     console.log('server running on port 3000')
+});
+
+// Resposta ao navegador após solicitação GET ao servidor
+app.get('/', (req, res) => {
+    res.send('Hello Word')
 });

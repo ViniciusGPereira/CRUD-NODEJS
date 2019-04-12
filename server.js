ead2700e2663bf91fb8a754d11006f1c788e4aef
ViniciusGPereira <vinicius.pereira.job@gmail.com>
2019-04-11 23:45:31 -0300

    MongoDB
    Importação do MongoDB
    Conexão com cloud.mongodb.com
    Criado o método CREATE = /show

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
+const uri = "mongodb+srv://admin:zGkpXvaWbAubBcfH@cluster-crudnodejs-dxxb9.mongodb.net/test?retryWrites=true";
+
+MongoClient.connect(uri, (err, client) => {
+    //Caso retorne algum erro mostra o erro no console
+    if(err) return console.log(err)
+    db = client.db('Cluster-CRUDNODEJS')
+    
+    //Iniciando servidor na porta 3000
+    app.listen(3000, () => {
+        console.log('Server running on port 3000')
+    })
+})
+
 
 //Iniciando a utilização do middleware Body Parser
 //Para trabalhar com a leitura dos dados do elemento <form> enviado
 //O comando urlencoded dentro do body parser diz ao mesmo para extrair dados do elemento <form>
 // E adicinar-los a propriedade BODY no objeto REQUEST
 app.use(bodyParser.urlencoded({extended: true}))
-
-// Iniciando servidor na porta 3000
-app.listen(3000, function(){
-    console.log('server running on port 3000')
-});
 
 // Resposta ao navegador após solicitação GET ao servidor
 app.get('/', (req, res) => {
@@ -31,5 +40,15 @@ app.set('view engine', 'ejs')
 //Enviada pelo formulário via action para /show
 app.post('/show', (req, res) => {
     //Exibição das propriedades contidas no body, pelo console
-    console.log(req.body)
+    //console.log(req.body)
+
+    //Cria uma coleção com nome data
+    //Uma coleção é um local nomeado para armazenar objetos
+    //Neste caso irei utilizar os objetos contidos no body que foram enviados via <form>
+    db.collection('data').save(req.body, (err, result) =>{
+        if (err) return console.log(err)
+
+        console.log('Salvo no BD')
+        res.redirect('/')
+    })    
 });
